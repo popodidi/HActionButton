35fc1438f03999fcc6a745c087e25573fe083484
Chang, Hao <popodidi@livemail.tw>
2016-09-02 11:47:01 +0800

    almost done

--- Example/HActionButton/ViewController.swift
+++ Example/HActionButton/ViewController.swift
@@ -7,18 +7,57 @@
 //
 
 import UIKit
+import HActionButton
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, HActionButtonDataSource, HActionButtonDelegate, HActionButtonAnimationDelegate {
 
+    @IBOutlet weak var actionButton: HActionButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        actionButton.dataSource = self
+        actionButton.delegate = self
+        actionButton.animationDelegate = self
+        actionButton.mainButton.setImage(UIImage(named:"red_dot"), forState: .Normal)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - HActionButtonDataSource
+    func numberOfItems(actionButton: HActionButton) -> Int {
+        return 20
+    }
+    // Optional
+    func actionButton(actionButton: HActionButton, itemButtonAtIndex index: Int) -> UIButton {
+        // return circle button with random color by default
+        return HActionButton.CircleItemButton(actionButton)
+        
+    }
+    // Optional
+    func actionButton(actionButton: HActionButton, relativeCenterPositionOfItemAtIndex index: Int) -> CGPoint{
+        // return circle button with random color by default
+        return HActionButton.EquallySpacedArcPosition(actionButton, atIndex: index, from: 0, to: 2 * M_PI)
+    }
+    
+    // MARK: - HActionButtonDelegate
+    func actionButton(actionButton: HActionButton, didClickItemButtonAtIndex index: Int) {
+        print("button \(index) clicked")
     }
 
+    // MARK: - HActionButtonAnimationDelegate
+    // Optional
+    func actionButton(actionButton: HActionButton, animationTimeForStatus active: Bool) -> NSTimeInterval {
+        //
+        return active ? 1.5 : 0.5
+    }
+    // Optional
+    func actionButton(actionButton: HActionButton, confugureMainButton mainButton: UIButton, forStatus active: Bool) {
+        // rotate button with PI/2 back and forth for status by default
+        HActionButton.RotateButton(mainButton, forStatus: active)
+        // HActionButton.RotateButton(mainButton, byAngle: M_PI/4, forStatus: active)
+    }
+    // Optional
+    func actionButton(actionButton: HActionButton, confugureItemButton itemButton: UIButton, atIndex index: Int, forStatus active: Bool) {
+        // Do whatever you want
+    }
 }
 
